491727410b74b9d7ead2864761779d639caa3185
d3dx13 <zhidkov2846@gmail.com>
2026-03-20 19:56:09 +0300

    task 1: first try

---
+++ src/solutions/task_1.rs
@@ -0,0 +1,46 @@
+#![allow(unused)]
+struct Solution;
+
+impl Solution {
+    /// [1. Two Sum](https://leetcode.com/problems/two-sum/)
+    ///
+    /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    ///
+    /// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    ///
+    /// You can return the answer in any order.
+    ///
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for index_l in 0..nums.len() {
+            for index_r in index_l + 1..nums.len() {
+                if nums[index_l] + nums[index_r] == target {
+                    return vec![index_l as i32, index_r as i32];
+                }
+            }
+        }
+        panic!("There are no such pairs")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        let ret = Solution::two_sum(vec![2, 7, 11, 15], 9);
+        assert_eq!(ret, vec![0, 1]);
+    }
+
+    #[test]
+    fn test_2() {
+        let ret = Solution::two_sum(vec![3, 2, 4], 6);
+        assert_eq!(ret, vec![1, 2]);
+    }
+
+    #[test]
+    fn test_3() {
+        let ret = Solution::two_sum(vec![3, 3], 6);
+        assert_eq!(ret, vec![0, 1]);
+    }
+}
